fad1bdaf6a30f9b7bd5f0e0761fd95e5474ad85d
amirali <ososgol@gmail.com>
2023-12-14 21:08:16 +0330

    add filter on product in shop

--- product/views.py
+++ product/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import DetailView, TemplateView, ListView
 
-from product.models import Product, Category
+from product.models import Product, Category, Size, Color
 
 
 class ProductDetailView(DetailView):
@@ -15,6 +15,7 @@ class NavbarePartialView(TemplateView):
     def get_context_data(self, **kwargs):
         contex = super(NavbarePartialView, self).get_context_data()  # super for get_contex_data
         contex['categories'] = Category.objects.all()
+
         return contex
 
 
@@ -24,6 +25,7 @@ class SampleCategoryView(TemplateView):
     def get_context_data(self, **kwargs):
         contex = super(SampleCategoryView, self).get_context_data()
         contex['categories'] = Category.objects.all()
+
         return contex
 
 
@@ -31,3 +33,44 @@ class ProductListView(ListView):
     template_name = 'product/shop.html'
     queryset = Product.objects.all()
 
+    def get_context_data(self, **kwargs):
+        context = super(ProductListView, self).get_context_data()
+        context['size'] = Size.objects.all()
+        context['color'] = Color.objects.all()
+
+        selected_colors = self.request.GET.getlist('color')
+        selected_sizes = self.request.GET.getlist('size')
+        min_price = self.request.GET.get('min-price')
+        max_price = self.request.GET.get('max-price')
+
+        final_queryset = Product.objects.all()
+        queryset = Product.objects.all()
+
+
+        if selected_colors:
+            queryset = Product.objects.filter(color__title__in=selected_colors).distinct()
+        if selected_sizes:
+            queryset = Product.objects.filter(size__title__in=selected_sizes).distinct()
+        if min_price and max_price:
+            queryset = Product.objects.filter(price__lte=max_price, price__gte=min_price)
+
+        if selected_colors or selected_sizes or (min_price is not None and max_price is not None):
+            final_queryset = Product.objects.none()
+
+            if selected_colors:
+                final_queryset = queryset.filter(color__title__in=selected_colors)
+
+            if selected_sizes:
+                final_queryset = final_queryset.filter(size__title__in=selected_sizes)
+
+            if min_price is not None and max_price is not None:
+                final_queryset = final_queryset.filter(price__range=(min_price, max_price))
+        context['object_list'] = final_queryset.distinct()
+
+        context['selected_colors'] = selected_colors
+        context['min_price'] = min_price
+        context['max_price'] = max_price
+        context['selected_sizes'] = selected_sizes
+
+        # print(selected_colors, selected_sizes, min_price, max_price)
+        return context
